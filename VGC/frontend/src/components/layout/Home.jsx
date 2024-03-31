9cd3c7b41f6489ab8e66d46365606265824198fd
KevinZheng0701 <kevinzheng700@gmail.com>
2024-03-31 18:58:11 -0400

    Resolved merging

--- VGC/frontend/src/components/layout/Home.jsx
+++ VGC/frontend/src/components/layout/Home.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <section className="landing">
-    <div className="dark-overlay">
-      <div className="landing-inner">
-        <h1 className="x-large">Video Game Catalog</h1>
-        <p className="lead">
-          Browse video games, leave reviews, and add to favorites
-        </p>
-        <div className="buttons">
-          <a href="register.html" className="btn btn-primary">Sign Up</a>
-          <a href="login.html" className="btn btn-light">Login</a>
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Video Game Catalog</h1>
+          <p className="lead">
+            Browse video games, leave reviews, and add to favorites
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
--- VGC/frontend/src/components/layout/Navbar.jsx
+++ VGC/frontend/src/components/layout/Navbar.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-
+import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
-    <h1>
-      <a href="index.html">VGC</a>
-    </h1>
-    <div className="navbar-search">
-        <input type="text" className="search-input" placeholder="Search..."></input>
-        <button type="submit" className="search-btn">Search</button>
-    </div>
-    <ul>
-      <li><a href="profiles.html">Games</a></li>
-      <li><a href="register.html">Register</a></li>
-      <li><a href="login.html">Login</a></li>
-    </ul>
-</nav>
-
+      <h1>
+        <a href="index.html">VGC</a>
+      </h1>
+      <div className="navbar-search">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search..."
+        ></input>
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </div>
+      <ul>
+        <li>
+          <a href="profiles.html">Games</a>
+        </li>
+        <li>
+          <a href="register.html">Register</a>
+        </li>
+        <li>
+          <a href="login.html">Login</a>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
 
 export default Navbar;
