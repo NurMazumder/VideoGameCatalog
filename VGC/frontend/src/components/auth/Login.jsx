984adf333932de0f233e1f65e3357ab1d2cdaf4e
NurMazumder <nmaz4828@gmail.com>
2024-03-31 22:10:21 -0400

    sign in page

--- VGC/frontend/src/components/auth/Login.jsx
+++ VGC/frontend/src/components/auth/Login.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const login = () => {
-  return <div>Login</div>;
+  return (
+    <React.Fragment>
+      <div className="form-wrapper">
+        <h2>Sign In</h2>
+        <form action="#">
+          <div className="form-control">
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+          <div className="form-control">
+            <input type="password" minLength="6" required />
+            <label>Password</label>
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+        <p>
+          Don't Have an Account? <a href="/Register">Sign Up</a>
+        </p>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default login;
