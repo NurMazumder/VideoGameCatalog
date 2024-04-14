809aba8793a1f4e061551da6db7501b0732c3e4e
NurMazumder <nmaz4828@gmail.com>
2024-04-14 14:16:34 -0400

    capatalization issue exporting model

--- models/GamesTest.js
+++ models/GamesTest.js
@@ -1,51 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const gameSchema = new Schema({
-    rawg_id: {
-        type: Number,
-        unique: true
+const GameSchema = new Schema({
+  rawg_id: {
+    type: Number,
+    unique: true,
+  },
+  game_name: {
+    type: String,
+    required: true,
+  },
+  game_background_image: {
+    type: String,
+  },
+  game_genres: {
+    genreId: {
+      type: Number,
+      unique: true,
     },
-    game_name: {
-        type: String, 
-        required: true
+    genreName: String,
+  },
+  game_released: {
+    type: Date,
+  },
+  game_tags: {
+    tagId: {
+      type: Number,
+      unique: true,
     },
-    game_background_image: {
-        type: String,
+    tagName: String,
+  },
+  game_rating: {
+    type: Number,
+  },
+  game_ratings_count: {
+    type: Number,
+  },
+  game_platforms: {
+    platformId: {
+      type: Number,
+      unique: true,
     },
-    game_genres: {
-        genreId:{
-            type: Number, 
-            unique: true
-        },
-        genreName: String
-    },
-    game_released: {
-        type: Date
-    },
-    game_tags : {
-        tagId:{
-            type: Number, 
-            unique: true
-        },
-        tagName: String
-    }, 
-    game_rating: {
-        type: Number
-    },
-    game_ratings_count: {
-        type: Number
-    },
-    game_platforms: {
-        platformId:{
-            type: Number, 
-            unique: true
-        },
-        platformName: String
-    },
-    game_esrb: {
-        type: String
-    }
-})
+    platformName: String,
+  },
+  game_esrb: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model("gamestests", GameSchema, "gamestests");
