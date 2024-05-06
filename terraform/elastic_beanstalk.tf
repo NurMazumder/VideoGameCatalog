47de85d7ddc63d5a1d45142b9b9695c6d92ceef1
KevinZheng0701 <kevinzheng700@gmail.com>
2024-05-06 00:08:45 -0400

    update beanstalk

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -6,7 +6,7 @@ resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "mernstack-environment"
   cname_prefix        = "kevinzheng0701mernstack"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Node.js 21"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
