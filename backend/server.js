385364771252ff1996a1fd869a8eca5f83821ef5
KevinZheng0701 <kevinzheng700@gmail.com>
2024-05-11 14:06:00 -0400

    Reorganized folders and files into a backend directory

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const path = require("path");
+const path = require("node:path");
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use("/api/games", require("./routes/api/gamesTest"));
 app.use("/api/wishlist", require("./routes/api/wishlist"));
 
 //serve static assets
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/dist"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
